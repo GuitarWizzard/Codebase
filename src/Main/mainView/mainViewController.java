199ddc792234fc3b2206a79e43bdff2272ab856a
Patiphon Suriyawong <louis_rich@windowslive.com>
2017-11-18 21:13:45 +0700

    add background to main ui

--- src/Main/mainView/mainViewController.java
+++ src/Main/mainView/mainViewController.java
@@ -1,6 +1,9 @@
 package Main.mainView;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 import slidingbar.SlidingBar;
 import slidingbar.Fingerboard;
@@ -9,7 +12,15 @@ import slidingbar.Fingerboard;
  * @author Louis Neen Oak Kaw
  */
 public class mainViewController {
+    ObservableList<String> songList = FXCollections.observableArrayList("Bigass","ThatWhatILike");
     
+    @FXML
+    private ComboBox songListBox;
+    
+    private void initialize() {
+        songListBox.setValue("Bigass");
+        songListBox.setItems(songList);
+    }
     @FXML
     public void test()
     {
