97a82492964371b547c542918bae15d7946d7e90
Patthawee <Patthawee.ch@hotmail.com>
2017-11-25 00:49:00 +0700

    All project again

---
+++ src/Database/Connection.java
@@ -0,0 +1,17 @@
+package Database;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connection {
+    public static EntityManagerFactory emf;
+    
+    public static void connect(){
+        emf = Persistence.createEntityManagerFactory("objectdb://161.246.6.25:6136/database.odb;user=admin;password=admin");
+        
+    }
+    
+    public static EntityManagerFactory getConnection(){
+        return emf;
+    }
+}
---
+++ src/Main/mainView/mainViewController.java
@@ -0,0 +1,40 @@
+package Main.mainView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import slidingbar.SlidingBar;
+/**
+ *
+ * @author Louis Neen Oak Kaw
+ */
+public class mainViewController implements Initializable{
+    ObservableList<String> songList = FXCollections.observableArrayList("Bigass","ThatWhatILike");
+    
+    @FXML
+    private ComboBox songListBox;
+    
+ 
+    @FXML
+    public void play()
+    {
+        SlidingBar songPlayer = new SlidingBar();
+        Stage stage = Main.Main.getPrimaryStage();
+        String song = (String) songListBox.getValue();
+        if(!song.equals(null))
+        {
+           songPlayer.play(stage,song); 
+        }
+        
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        songListBox.setItems(songList);
+    }
+}
